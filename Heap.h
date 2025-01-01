5358641a3ceb75843c8d379c6cfeaa987ca67898
xGogax <lacastanko03@gmail.com>
2025-01-02 00:29:03 +0100

    Working Heap

--- Heap.h
+++ Heap.h
@@ -16,35 +16,8 @@ public:
         delete[] data;
     }
 
-    void insert(int value) {
-        //prazan
-        if(size == 0) {
-            data = new int[1];
-        } else {
-            //nije prazan = prosirenje
-            int* newData = new int[size + 1];
-            for(int i = 0; i < size; i++) {
-                newData[i] = data[i];
-            }
-            delete[] data;
-            data = newData;
-        }
-
-        data[size++] = value;
-        heapify(size-1);
-    }
-
-    void printHeap() const {
-        if (isEmpty()) {
-            cout << "Heap is empty!" << endl;
-            return;
-        }
-        cout << "Heap elements: ";
-        for (int i = 1; i <= size; ++i) {
-            cout << data[i-1] << " ";
-        }
-        cout << endl;
-    }
+    void insert(int value);
+    void printHeap() const;
 
 private:
     int* data;
@@ -55,31 +28,7 @@ private:
         return size == 0;
     }
 
-    void heapify(int index) {
-        int parent = (index - 1) / 2;
-
-        while(index > 0) {
-            cout << "unos: " << data[index] << endl;
-            cout << "njegov otac: " << data[parent] << endl;
-            if(isMaxHeap) {
-                if(data[index] > data[parent]) {
-                    swap(data[index], data[parent]);
-                    index = parent;
-                    parent = (index - 1) / 2;
-                } else {
-                    break;
-                }
-            } else {
-                if(data[index] < data[parent]) {
-                    swap(data[index], data[parent]);
-                    index = parent;
-                    parent = (index - 1) / 2;
-                } else {
-                    break;
-                }
-            }
-        }
-    }
+    void heapify(int index);
 };
 
 #endif // HEAP_H
--- main.cpp
+++ main.cpp
@@ -5,15 +5,5 @@
 using namespace std;
 
 int main() {
-    Heap maxHeap(false);
-    maxHeap.insert(89);
-    maxHeap.insert(46);
-    maxHeap.insert(50);
-    maxHeap.insert(22);
-    maxHeap.insert(24);
-    maxHeap.insert(2);
-    maxHeap.insert(93);
-    maxHeap.printHeap();
-
     return 0;
 }
